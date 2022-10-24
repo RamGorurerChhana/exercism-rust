fef775a072b7a45bad7dc30803d09322c6e06264
Sibaprasad Maiti <sibu.it13@gmail.com>
2022-10-25 00:59:32 +0530

    poker completed

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -1,128 +1,340 @@
+use std::cmp::Ordering;
+use std::str::FromStr;
+
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
-    let mut hand = hands.to_vec();
+    // create Hand for each input strings
+    let mut hands = hands.iter().map(|&h| Hand::new(h)).collect::<Vec<_>>();
+    // sort the hands by descending order of ranks
+    hands.sort_unstable_by(|a, b| b.rank.partial_cmp(&a.rank).unwrap());
+    let mut v = vec![];
+    // take from the beginning and continue until hands are equal rank
+    for i in 0..hands.len() {
+        if i == 0 || hands[i - 1].rank == hands[i].rank {
+            v.push(hands[i].hand_str);
+        } else {
+            break;
+        }
+    }
 
-    hand.sort();
-
-    hand
+    v
 }
 
+type CardValue = u8;
+type FiveCards = [Card; 5];
 
-// Poker hands with different categories ranking (from high to low)
-// Straight Flash 
-// Four of a kind 
-// Full House
-// Flush
-// Straight
-// Three of a kind
-// Two Pair
-// One Pair
-// High Card
+#[derive(Debug, PartialEq)]
+enum Rank {
+    StraightFlush(CardValue),
+    FourOfAKind(CardValue, CardValue),
+    FullHouse(CardValue, CardValue),
+    Flush(Box<Rank>),
+    Straight(CardValue),
+    ThreeOfAKind(CardValue, CardValue, CardValue),
+    TwoPair(CardValue, CardValue, CardValue),
+    OnePair(CardValue, CardValue, CardValue, CardValue),
+    HighCard(CardValue, CardValue, CardValue, CardValue, CardValue),
+}
 
-// If both hands have Straight Flash then highest card value from both hands are compoared. If both hands have same high card then its tie. 
-// If both hands have Four of a kind then card value of the common cards are compared. If common card value are same then value of the remaining card matters. 
-// If both hands have Full House then value of 3 of a kind card compared first, then value of 2 of a kind card compared. 
-// If both hands have Flush then value of the high card compared followed by next high card. 
-// If both hands have Straight then value of the high card compared followed by next high card.
-// If both hands have Three of a kind then value of the common card compared first, followed by value of the high card
-// If both hands have two pairs then value of common cards are compared first followed by next high card 
-// If both hands have one pair then value of common cards are compared first followed by next high card
-// If both hands have only high cards then value of the high cards are compared followed by next high card 
+/// implement PartialOrd for Rank so that hands can be sorted
+/// based on their ranks
+impl PartialOrd for Rank {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            // both hands have Straigt Flush
+            (Self::StraightFlush(v1), Self::StraightFlush(v2)) => Some(v1.cmp(v2)),
+            // one hand has straight Flush
+            (Self::StraightFlush(_), _) => Some(Ordering::Greater),
+            (_, Self::StraightFlush(_)) => Some(Ordering::Less),
+            // Both hands have FourOfAKind
+            (Self::FourOfAKind(m1, m2), Self::FourOfAKind(n1, n2)) => {
+                ordering_multiple_cards(&[(*m1, *n1), (*m2, *n2)])
+            }
+            // one hand has FourOfAKind
+            (Self::FourOfAKind(_, _), _) => Some(Ordering::Greater),
+            (_, Self::FourOfAKind(_, _)) => Some(Ordering::Less),
+            // both hands have FullHouse
+            (Self::FullHouse(m1, m2), Self::FullHouse(n1, n2)) => {
+                ordering_multiple_cards(&[(*m1, *n1), (*m2, *n2)])
+            }
+            // only one hand has FullHouse
+            (Self::FullHouse(_, _), _) => Some(Ordering::Greater),
+            (_, Self::FullHouse(_, _)) => Some(Ordering::Less),
+            // Both hands have Flash
+            (Self::Flush(b1), Self::Flush(b2)) => b1.partial_cmp(b2),
+            // Only one hand has Flush
+            (Self::Flush(_), _) => Some(Ordering::Greater),
+            (_, Self::Flush(_)) => Some(Ordering::Less),
+            // Both hands have Straight
+            (Self::Straight(v1), Self::Straight(v2)) => Some(v1.cmp(v2)),
+            // Only one hand has Straight
+            (Self::Straight(_), _) => Some(Ordering::Greater),
+            (_, Self::Straight(_)) => Some(Ordering::Less),
+            // Both hands have ThreeOfAKind
+            (Self::ThreeOfAKind(m1, m2, m3), Self::ThreeOfAKind(n1, n2, n3)) => {
+                ordering_multiple_cards(&[(*m1, *n1), (*m2, *n2), (*m3, *n3)])
+            }
+            // Only one hand has ThreeOfAKind
+            (Self::ThreeOfAKind(_, _, _), _) => Some(Ordering::Greater),
+            (_, Self::ThreeOfAKind(_, _, _)) => Some(Ordering::Less),
+            // Both hands have TwoPair
+            (Self::TwoPair(m1, m2, m3), Self::TwoPair(n1, n2, n3)) => {
+                ordering_multiple_cards(&[(*m1, *n1), (*m2, *n2), (*m3, *n3)])
+            }
+            // only one hand has TwoPair
+            (Self::TwoPair(_, _, _), _) => Some(Ordering::Greater),
+            (_, Self::TwoPair(_, _, _)) => Some(Ordering::Less),
+            // Both hands have OnePair
+            (Self::OnePair(m1, m2, m3, m4), Self::OnePair(n1, n2, n3, n4)) => {
+                ordering_multiple_cards(&[(*m1, *n1), (*m2, *n2), (*m3, *n3), (*m4, *n4)])
+            }
+            // Only one hands have OnePair
+            (Self::OnePair(_, _, _, _), _) => Some(Ordering::Greater),
+            (_, Self::OnePair(_, _, _, _)) => Some(Ordering::Less),
+            // Both hands have HighCard
+            (Self::HighCard(m1, m2, m3, m4, m5), Self::HighCard(n1, n2, n3, n4, n5)) => {
+                ordering_multiple_cards(&[
+                    (*m1, *n1),
+                    (*m2, *n2),
+                    (*m3, *n3),
+                    (*m4, *n4),
+                    (*m5, *n5),
+                ])
+            }
+        }
+    }
+}
+// ordering multiple pairs of cards
+// comparing one pair at a time
+fn ordering_multiple_cards(cards: &[(CardValue, CardValue)]) -> Option<Ordering> {
+    for card in cards {
+        match card.0.cmp(&card.1) {
+            Ordering::Greater => return Some(Ordering::Greater),
+            Ordering::Less => return Some(Ordering::Less),
+            Ordering::Equal => {}
+        };
+    }
+    Some(Ordering::Equal)
+}
 
+impl Rank {
+    fn assign_rank(cards: &FiveCards) -> Self {
+        let has_flush = is_flush(cards);
+        let mut all_values = cards.iter().map(|c| c.value).collect::<Vec<_>>();
+        all_values.sort_unstable();
+        // Check for Straight Flush and Straight
+        if let Some(v) = check_straight(&all_values) {
+            if has_flush {
+                return Self::StraightFlush(v);
+            } else {
+                return Self::Straight(v);
+            }
+        }
+        // check FourOfAKind
+        if let Some((v1, v2)) = check_four_of_a_kind(&all_values) {
+            return Self::FourOfAKind(v1, v2);
+        }
+        // check FullHouse
+        if let Some((v1, v2)) = check_full_house(&all_values) {
+            return Self::FullHouse(v1, v2);
+        }
 
-// Enum Rank
-// StraightFlush(u8) -- value denoting the high card value
-// FourOfAKind(u8, u8) -- first value of the common card, second value of the remaining card
-// FullHouse(u8, u8) - first value of 3 common card, second value of pair card
-// Flush(Box<Rank>) - value points to a rank (E.g- Flush(Box<TwoPair(9, 4, 2)>))
-// Straight(u8) - value denoting the high card value 
-// TheeOfAKind(u8, u8, u8) - common card value, next high card, remaining card
-// TwoPair(u8, u8, u8) - high pair card value, next pair card value, remaining card
-// OnePair(u8, u8, u8, u8) - pair card value, next high card, next high card, next high card
-// HighCard(u8, u8, u8, u8, u8) - card values from high to low
+        // check ThreeOfAKind
+        let rank = if let Some((v1, v2, v3)) = check_three_of_a_kind(&all_values) {
+            Self::ThreeOfAKind(v1, v2, v3)
+            // check TwoPair
+        } else if let Some((v1, v2, v3)) = check_two_pairs(&all_values) {
+            Self::TwoPair(v1, v2, v3)
+            // check OnePair
+        } else if let Some((v1, v2, v3, v4)) = check_one_pair(&all_values) {
+            Self::OnePair(v1, v2, v3, v4)
+        } else {
+            // default HighCard
+            Self::HighCard(
+                all_values[4],
+                all_values[3],
+                all_values[2],
+                all_values[1],
+                all_values[0],
+            )
+        };
+        if has_flush {
+            Self::Flush(Box::new(rank))
+        } else {
+            rank
+        }
+    }
+}
 
+// if all 5 cards have same suite
+fn is_flush(cards: &FiveCards) -> bool {
+    cards.iter().all(|c| c.suite == cards[0].suite)
+}
 
-// Enum to denote Card
-// Card
-// Spades(CardValue)
-// Hearts(CardValue)
-// Diamonds(CardValue)
-// Clubs(CardValue)
+// check if has straight
+// cards are sorted by CardValue ascending
+fn check_straight(values: &[CardValue]) -> Option<CardValue> {
+    // attempt to make a straight
+    if values[0] + 1 == values[1]
+        && values[1] + 1 == values[2]
+        && values[2] + 1 == values[3]
+        && values[3] + 1 == values[4]
+    {
+        return Some(values[4]);
+    }
+    // attempt to make an aces with putting ace = 1
+    if [2, 3, 4, 5, 14].into_iter().all(|n| values.contains(&n)) {
+        return Some(5);
+    }
+    // straight not possible
+    None
+}
 
-// Enum to denote card value
-// CardValue
-// One = 1
-// Two = 2
-// Three = 3
-// Four = 4
-// Five = 5
-// Six = 6
-// Seven = 7
-// Eight = 8
-// Nine = 9
-// Ten = 10
-// Jack = 11
-// Queen = 12
-// King = 13
-// Ace = 14
+// check FourOfAKind
+// cards are sorted by CardValue ascending
+fn check_four_of_a_kind(values: &[CardValue]) -> Option<(CardValue, CardValue)> {
+    if values[0] == values[3] {
+        return Some((values[0], values[4]));
+    }
+    if values[1] == values[4] {
+        return Some((values[1], values[0]));
+    }
 
-// struct to denote Hand 
-// Hand
-// cards: [Card;5],
-// rank: Rank
+    None
+}
 
+// check FullHouse
+// cards are sorted by CardValue ascending
+fn check_full_house(values: &[CardValue]) -> Option<(CardValue, CardValue)> {
+    if values[0] == values[2] && values[3] == values[4] {
+        return Some((values[0], values[3]));
+    }
+    if values[0] == values[1] && values[2] == values[4] {
+        return Some((values[2], values[0]));
+    }
 
-// TIPS: Since Ace can be treated as Ace and one both
-// we can consider as two cards and take the best 5 cards out of 6 cards
+    None
+}
 
-// panic incase of errorneous input
-// not putting any error handling now
+// check ThreeOfAKind
+// cards are sorted by CardValue ascending
+fn check_three_of_a_kind(values: &[CardValue]) -> Option<(CardValue, CardValue, CardValue)> {
+    if values[0] == values[2] {
+        Some((values[0], values[4], values[3]))
+    } else if values[1] == values[3] {
+        Some((values[1], values[4], values[0]))
+    } else if values[2] == values[4] {
+        Some((values[2], values[1], values[0]))
+    } else {
+        None
+    }
+}
 
-// Parsing Strategy:
-// all slice of &str will have exactly 5 &str when splitted by <space>, otherwise panic
-// all &str will be either 2 chars or 3 chars 
-// last char is suite
-// remaining chars from the begininng will denote value 
-// Implement FromStr trait for CardValue 
-// Implement FromStr trait for Card which will internally get CardValue 
+// check TwoPairs
+// cards are sorted by CardValue ascending
+fn check_two_pairs(values: &[CardValue]) -> Option<(CardValue, CardValue, CardValue)> {
+    if values[0] == values[1] && values[2] == values[3] {
+        Some((values[2], values[0], values[4]))
+    } else if values[1] == values[2] && values[3] == values[4] {
+        Some((values[3], values[1], values[0]))
+    } else if values[0] == values[1] && values[3] == values[4] {
+        Some((values[3], values[1], values[2]))
+    } else {
+        None
+    }
+}
 
-// Implement FromStr for Hand struct
-// which will first construct array of card [Card;5]
-// then calculate the rank of the hand
-// instantiate the Hand
+// check OnePair
+// cards are sorted by CardValue ascending
+fn check_one_pair(values: &[CardValue]) -> Option<(CardValue, CardValue, CardValue, CardValue)> {
+    if values[0] == values[1] {
+        Some((values[0], values[4], values[3], values[2]))
+    } else if values[1] == values[2] {
+        Some((values[1], values[4], values[3], values[0]))
+    } else if values[2] == values[3] {
+        Some((values[2], values[4], values[1], values[0]))
+    } else if values[3] == values[4] {
+        Some((values[3], values[2], values[1], values[0]))
+    } else {
+        None
+    }
+}
 
-// Implement PartialEq for Hand struct 
-// so that it can be compared while sorting 
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Suite {
+    Spades,
+    Hearts,
+    Diamonds,
+    Clubs,
+}
 
-// sort the original Vec<&str> ascending passing a function to sort_by function 
-// which will convert both &str to Hand and then compare 
-//
-// create a new Vec<&str> to return 
-// pop a item from the vec if there is no item already in the new vec then insert
-// pop again compare the value with the item in the vec 
-// if low then break and return 
-// otherwise insert and continue again 
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Card {
+    suite: Suite,
+    value: CardValue,
+}
 
-// That's pretty much how the logic would go. 
-// HARE KRISHNA. 🙏 
-// GOD SAVE ME. 😭 
+impl Card {
+    fn new(suite: Suite, value: CardValue) -> Self {
+        Self { suite, value }
+    }
+}
 
+/// Create a card instance from the given string
+/// it will assign the card value and the suite
+impl FromStr for Card {
+    type Err = &'static str;
 
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // except last char take from the beginning
+        let value_str = &s[0..s.len() - 1];
+        // last char represents the suite
+        let suite = &s[s.len() - 1..s.len()];
+        // cards from 2 - 10 will parse successfully
+        // other 4 cards assign value manually
+        let value = match value_str.parse::<CardValue>() {
+            Ok(v) => v,
+            Err(_) => match value_str {
+                "J" => 11,
+                "Q" => 12,
+                "K" => 13,
+                "A" => 14,
+                _ => unreachable!(),
+            },
+        };
+        // match suite and create Card instance
+        match suite {
+            "S" => Ok(Self::new(Suite::Spades, value)),
+            "H" => Ok(Self::new(Suite::Hearts, value)),
+            "D" => Ok(Self::new(Suite::Diamonds, value)),
+            "C" => Ok(Self::new(Suite::Clubs, value)),
+            _ => Err("unreachable"),
+        }
+    }
+}
 
+#[derive(Debug)]
+struct Hand<'a> {
+    hand_str: &'a str,
+    rank: Rank,
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+impl<'a> Hand<'a> {
+    fn new(hand_str: &'a str) -> Self {
+        // each hand contains 5 cards
+        // split by ' ' and then convert to FiveCards
+        let hands = hand_str
+            .split(' ')
+            .map(|s| Card::from_str(s).unwrap())
+            .collect::<Vec<_>>();
+        if hands.len() != 5 {
+            panic!("Invalid hand");
+        }
+        // assign the highest possible rank for the Hand
+        let rank = Rank::assign_rank(&[hands[0], hands[1], hands[2], hands[3], hands[4]]);
+        Self { hand_str, rank }
+    }
+}
